227ffbfbdd7def5e5a037d9c02adf4d103fa6dc2
abelembaye <abel.embaye@gmail.com>
2024-04-02 22:43:15 -0500

    on 4.2.2024b

--- pages/Econ3333-regression.py
+++ pages/Econ3333-regression.py
@@ -84,53 +84,53 @@ else:
     else:
         st.warning("Please Upload Dataset")
 
-    # 3. Show Dataset
-    if upload is not None:
-        if st.checkbox("Preview Dataset"):
-            if st.button("Head"):
-                st.write(data.head())
-            if st.button("Tail"):
-                st.write(data.tail())
+    # # 3. Show Dataset
+    # if upload is not None:
+    #     if st.checkbox("Preview Dataset"):
+    #         if st.button("Head"):
+    #             st.write(data.head())
+    #         if st.button("Tail"):
+    #             st.write(data.tail())
 
-    # 4. Check DataType of Each Column
-    if upload is not None:
-        if st.checkbox("DataType of Each Column"):
-            st.text("DataTypes")
-            st.write(data.dtypes)
+    # # 4. Check DataType of Each Column
+    # if upload is not None:
+    #     if st.checkbox("DataType of Each Column"):
+    #         st.text("DataTypes")
+    #         st.write(data.dtypes)
 
-    # 5. Find Shape of Our Dataset (Number of Rows And Number of Columns)
-    if upload is not None:
-        data_shape = st.radio("What Dimension Do You Want To Check?", ('Rows',
-                                                                       'Columns'))
-        if data_shape == 'Rows':
-            st.text("Number of Rows")
-            st.write(data.shape[0])
-        if data_shape == 'Columns':
-            st.text("Number of Columns")
-            st.write(data.shape[1])
+    # # 5. Find Shape of Our Dataset (Number of Rows And Number of Columns)
+    # if upload is not None:
+    #     data_shape = st.radio("What Dimension Do You Want To Check?", ('Rows',
+    #                                                                    'Columns'))
+    #     if data_shape == 'Rows':
+    #         st.text("Number of Rows")
+    #         st.write(data.shape[0])
+    #     if data_shape == 'Columns':
+    #         st.text("Number of Columns")
+    #         st.write(data.shape[1])
 
-    # 6. Find Null Values in The Dataset
-    if upload is not None:
-        test = data.isnull().values.any()
-        if test == True:
-            if st.checkbox("Null Values in the dataset"):
-                sns.heatmap(data.isnull())
-                st.pyplot()
-        else:
-            st.success("Congratulations!!!,No Missing Values")
+    # # 6. Find Null Values in The Dataset
+    # if upload is not None:
+    #     test = data.isnull().values.any()
+    #     if test == True:
+    #         if st.checkbox("Null Values in the dataset"):
+    #             sns.heatmap(data.isnull())
+    #             st.pyplot()
+    #     else:
+    #         st.success("There are no Missing Values in the dataset")
 
-    # 7. Find Duplicate Values in the dataset
-    if upload is not None:
-        test = data.duplicated().any()
-        if test == True:
-            st.warning("This Dataset Contains Some Duplicate Values")
-            dup = st.selectbox("Do You Want to Remove Duplicate Values?",
-                               ("Select One", "Yes", "No"))
-            if dup == "Yes":
-                data = data.drop_duplicates()
-                st.text("Duplicate Values are Removed")
-            if dup == "No":
-                st.text("Ok No Problem")
+    # # 7. Find Duplicate Values in the dataset
+    # if upload is not None:
+    #     test = data.duplicated().any()
+    #     if test == True:
+    #         st.warning("This Dataset Contains Some Duplicate Values")
+    #         dup = st.selectbox("Do You Want to Remove Duplicate Values?",
+    #                            ("Select One", "Yes", "No"))
+    #         if dup == "Yes":
+    #             data = data.drop_duplicates()
+    #             st.text("Duplicate Values are Removed")
+    #         if dup == "No":
+    #             st.text("Ok No Problem")
 
     # if st.button('Save DataFrame'):
     #     open('C:/Users/data_streamlit.csv', 'w').write(data.to_csv())
@@ -142,6 +142,7 @@ else:
             st.write(data.describe(include='all'))
 
     # 9. Multiple Regression Analysis
+
     if upload is not None:
         if st.checkbox("Perform Multiple Regression Analysis"):
             st.text("Dependent Variable")
@@ -154,6 +155,10 @@ else:
             if len(independent_vars) > 0 and dependent_var:
                 X = data[independent_vars]
                 y = data[dependent_var]
+
+                # Drop rows with missing values
+                X = X.dropna()
+                y = y.loc[X.index]
 
                 X = sm.add_constant(X)  # adding a constant
 
